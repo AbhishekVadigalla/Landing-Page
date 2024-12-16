a8eb60a23b4a69673af514e5a03ed97bb8df5ff6
Abhishek Vadigalla <abhishekvadigalla@gmail.com>
2024-12-16 18:29:18 +0530

    Landing Page in to GitHub

---
+++ cosmos/src/App.js
@@ -0,0 +1,126 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+function App() {
+  return (
+    <div>
+      <div className='header'>
+      <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand">Cosmos</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+      </div>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='side-nav col-1 '>
+          <ul class="list-group">
+  <li class="list-group-item">Home</li>
+  <li class="list-group-item">DashBoard</li>
+  <li class="list-group-item">About</li>
+  <li class="list-group-item">Help</li>
+  <li class="list-group-item">Contact US</li>
+  <li class="list-group-item">Rate us</li>
+  <li class="list-group-item">E-mail</li>
+  <li class="list-group-item">LogOut</li>
+</ul>
+          </div>
+          <div className='Main-content col-11'>
+            <h2>MarketPlace</h2>
+            <div className='row pt-2'>
+              <div className='col-4 p-4'>
+              <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              
+            </div>
+              </div>
+              <div className='col-4 p-4'>
+              <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
